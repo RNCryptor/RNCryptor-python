2406ff5655666d5715282f32eed31ea40370fea1
Yan Kalchevksiy <yan.kalchevskiy@gmail.com>
2016-04-02 02:09:37 +0300

    Add tests that check raising DecryptionError on bad data

--- tests.py
+++ tests.py
@@ -12,6 +12,10 @@ DATA = (
     '2' * 15,
     '3' * 17,
 )
+BAD_DATA = (
+    'x' * 100,
+    'y' * 100,
+)
 PASSWORD_DATA = (
     'p@s$VV0Rd',
     'пароль',
@@ -33,3 +37,10 @@ def test_encrypt_decrypt_functions_should_be_correct(data, password):
     encrypted_data = rncryptor.encrypt(data, password)
     decrypted_data = rncryptor.decrypt(encrypted_data, password)
     assert data == decrypted_data
+
+
+@pytest.mark.parametrize('data', BAD_DATA)
+@pytest.mark.parametrize('password', PASSWORD_DATA)
+def test_decryption_bad_data_should_raise_exception(data, password):
+    with pytest.raises(rncryptor.DecryptionError):
+        rncryptor.decrypt(data, password)
