f5f4bbdaeb11084f986b5670a91474ea88fca533
Alin Radut <alin.claudiu.radut@gmail.com>
2024-11-26 16:54:50 +0200

    Add support for encryption and decryption using HMAC and encryption keys, add header sanity checks

--- test_rncryptor.py
+++ test_rncryptor.py
@@ -19,6 +19,10 @@ PASSWORD_DATA = (
     'p@s$VV0Rd',
     'пароль',
 )
+SALT = (
+    'lPVVIl6Z',
+    'TSDTe9c6',
+)
 
 
 @pytest.mark.parametrize('data', DATA)
@@ -41,5 +45,76 @@ def test_encrypt_decrypt_functions_should_be_correct(data, password):
 @pytest.mark.parametrize('data', BAD_DATA)
 @pytest.mark.parametrize('password', PASSWORD_DATA)
 def test_decryption_bad_data_should_raise_exception(data, password):
+    with pytest.raises(rncryptor.DecryptionError):
+        rncryptor.decrypt(data, password)
+
+
+@pytest.mark.parametrize('encryption_salt', SALT)
+@pytest.mark.parametrize('hmac_salt', SALT)
+@pytest.mark.parametrize('data', DATA)
+@pytest.mark.parametrize('password', PASSWORD_DATA)
+def test_enc_dec_with_keys_methods_should_be_correct(encryption_salt, hmac_salt, data, password):
+    cryptor = rncryptor.RNCryptor()
+    encryption_key = cryptor.make_key(password, encryption_salt)
+    hmac_key = cryptor.make_key(password, hmac_salt)
+    encrypted_data = cryptor.encrypt_with_keys(data, hmac_key, encryption_key)
+    decrypted_data = cryptor.decrypt_with_keys(encrypted_data, hmac_key, encryption_key)
+    assert data == decrypted_data
+
+
+@pytest.mark.parametrize('encryption_salt', SALT)
+@pytest.mark.parametrize('hmac_salt', SALT)
+@pytest.mark.parametrize('data', DATA)
+@pytest.mark.parametrize('password', PASSWORD_DATA)
+def test_enc_dec_with_keys_functions_should_be_correct(encryption_salt, hmac_salt, data, password):
+    encryption_key = rncryptor.make_key(password, encryption_salt)
+    hmac_key = rncryptor.make_key(password, hmac_salt)
+    encrypted_data = rncryptor.encrypt_with_keys(data, hmac_key, encryption_key)
+    decrypted_data = rncryptor.decrypt_with_keys(encrypted_data, hmac_key, encryption_key)
+    assert data == decrypted_data
+
+
+@pytest.mark.parametrize('encryption_salt', SALT)
+@pytest.mark.parametrize('hmac_salt', SALT)
+@pytest.mark.parametrize('data', DATA)
+@pytest.mark.parametrize('password', PASSWORD_DATA)
+def test_enc_keys_dec_pass_func_should_raise_exception(encryption_salt, hmac_salt, data, password):
+    # test encryption with keys and decryption with password. should because of the header mismatch.
+    # should raise an error because of the header mismatch.
+    encryption_key = rncryptor.make_key(password, encryption_salt)
+    hmac_key = rncryptor.make_key(password, hmac_salt)
+    encrypted_data = rncryptor.encrypt_with_keys(data, hmac_key, encryption_key)
+    with pytest.raises(rncryptor.DecryptionError):
+        rncryptor.decrypt(encrypted_data, password)
+
+
+@pytest.mark.parametrize('encryption_salt', SALT)
+@pytest.mark.parametrize('hmac_salt', SALT)
+@pytest.mark.parametrize('data', DATA)
+@pytest.mark.parametrize('password', PASSWORD_DATA)
+def test_enc_pass_dec_keys_func_should_raise_exception(encryption_salt, hmac_salt, data, password):
+    # test encryption with password and decryption with keys.
+    # should raise an error because of the header mismatch.
+    encryption_key = rncryptor.make_key(password, encryption_salt)
+    hmac_key = rncryptor.make_key(password, hmac_salt)
+    encrypted_data = rncryptor.encrypt(data, password)
+    with pytest.raises(rncryptor.DecryptionError):
+        rncryptor.decrypt_with_keys(encrypted_data, hmac_key, encryption_key)
+
+
+@pytest.mark.parametrize('data', BAD_DATA)
+@pytest.mark.parametrize('password', PASSWORD_DATA)
+def test_decryption_short_header_should_raise_exception(data, password):
+    # minimum length is 66 bytes
+    data = data[65:]
+    with pytest.raises(rncryptor.DecryptionError):
+        rncryptor.decrypt(data, password)
+
+
+@pytest.mark.parametrize('data', BAD_DATA)
+@pytest.mark.parametrize('password', PASSWORD_DATA)
+def test_decryption_with_keys_short_header_should_raise_exception(data, password):
+    # minimum length is 50 bytes
+    data = data[49:]
     with pytest.raises(rncryptor.DecryptionError):
         rncryptor.decrypt(data, password)
